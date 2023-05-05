25b774ffb8bc8fb04bf76154f2f0ba08416c5645
false <vlad@live.no>
2023-05-05 10:02:55 +0100

    init commit

---
+++ simple_pro/src/main/java/com/ericsson/simple_pro/SimpleProApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.simple_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleProApplication.class, args);
+    }
+
+}
